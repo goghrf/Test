3424b7f6c803a21b95bd0856106d3736fd6c257a
blurfree <blurfree2521@gmail.com>
2024-09-18 16:16:52 +0900

    test

---
+++ Test/Test/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Test
+//
+//  Created by KoSungmin on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
